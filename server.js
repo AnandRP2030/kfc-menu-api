d01836b3f2280f62814b4518ef067634f92a5a39
Anand R P <anand.rp2030@gmail.com>
2023-01-31 01:44:28 +0530

    kfc menu json data

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000; // you can use any port number here; i choose to use 3001
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
